e38630d4181801225baae7785fcfebd5e52d044a
Achilleas Tsoumitas <knorcedger@gmail.com>
2017-03-05 23:59:03 +0000

    Added home (not ready)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './home/home';
 import Leaderboard from './leaderboard/leaderboard';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom';
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
 
 class App extends Component {
   render() {
---
+++ src/home/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './home.css';
+import '../common/xp.css';
+// import photo from '../leaderboard/mark.jpg';
+
+class Leaderboard extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <span className='xp'>
+          <span className='text'>{14}</span>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Leaderboard;
