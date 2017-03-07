c39cc8eb4a7542d81b6dc7b3715cc60c67f3e4d6
Achilleas Tsoumitas <knorcedger@gmail.com>
2017-03-07 00:27:07 +0000

    Added the temp view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './home/home';
 import Leaderboard from './leaderboard/leaderboard';
+import Temp from './temp/temp';
 import {
   BrowserRouter as Router,
   Route,
@@ -16,12 +17,14 @@ class App extends Component {
           <ul>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/leaderboard'>Leaderboard</Link></li>
+            <li><Link to='/temp'>Temp</Link></li>
           </ul>
 
           <hr/>
 
           <Route exact path='/' component={Home} />
           <Route path='/leaderboard' component={Leaderboard} />
+          <Route path='/temp' component={Temp} />
         </div>
       </Router>
     );
---
+++ src/temp/temp.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './temp.css';
+import XP from '../components/XP/XP';
+import Hexagon from '../components/Hexagon/Hexagon';
+import badge from '../assets/badge.svg';
+import trophy from '../assets/trophy.svg';
+
+class Leaderboard extends Component {
+  render() {
+    return (
+      <div className='temp-container'>
+        <XP level={14} />
+        <Hexagon><img src={badge} width='35' alt='badge' /></Hexagon>
+        <Hexagon bg='red'><img src={trophy} width='35' alt='trophy' /></Hexagon>
+      </div>
+    );
+  }
+}
+
+export default Leaderboard;
