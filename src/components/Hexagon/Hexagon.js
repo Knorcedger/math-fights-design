ccfa50d80ea3c81dd141875c0038c8beee18146b
Achilleas Tsoumitas <knorcedger@gmail.com>
2017-03-11 16:55:51 +0000

    Added aphrodite. Hexagon bar

--- src/components/Hexagon/Hexagon.js
+++ src/components/Hexagon/Hexagon.js
@@ -1,23 +1,93 @@
+/*http://csshexagon.com/*/
+
 import React, {PropTypes} from 'react';
-import classNames from 'classnames';
-classNames('foo', 'bar'); // => 'foo bar'
+import {StyleSheet, css} from 'aphrodite';
 
-import './Hexagon.css';
+const Hexagon = ({bar, barColor, barPercent, barSize, mainColor, size, children}) => {
+  const hexagonStyle = getHexagonStyle(size, mainColor, barSize, barColor);
+  const classes = css(
+    hexagonStyle.hexagon,
+    bar && hexagonStyle.bar,
+  );
 
-const Hexagon = ({bg, children}) => {
-  const classes = classNames({
-    'hexagon': true,
-    [`${bg}`]: bg
-  });
+  // calculate the correct percent relative to the size
+  const relativeBarPercent = (barSize / size) * barPercent;
+  const barElement = bar ? (<span className={css(getBarStyle(relativeBarPercent, mainColor, size).bar)}></span>) : '';
+
   return (
     <span className={classes}>
-      <span className='content'>{children}</span>
+      <span className={css(styles.content)}>{children}</span>
+      {barElement}
     </span>
   );
 }
 
 Hexagon.props = {
-  bg: PropTypes.string
+  bar: PropTypes.bool,
+  barColor: PropTypes.string,
+  barPercent: PropTypes.number,
+  barSize: PropTypes.number,
+  mainColor: PropTypes.string,
+  size: PropTypes.number
 };
+
+const getHexagonStyle = (size, mainColor, barSize, barColor) => {
+  return StyleSheet.create({
+    hexagon: {
+      position: 'relative',
+      width: size,
+      height: size * 0.5775,
+      backgroundColor: mainColor,
+      margin: `${(size * 0.5775) / 2}px 0`,
+      ':before': {
+        content: '""',
+        position: 'absolute',
+        width: 0,
+        borderLeft: `${size / 2}px solid transparent`,
+        borderRight: `${size / 2}px solid transparent`,
+        bottom: '100%',
+        borderBottom: `${(size * 0.5775) / 2}px solid ${mainColor}`
+      },
+      ':after': {
+        content: '""',
+        position: 'absolute',
+        width: 0,
+        borderLeft: `${size / 2}px solid transparent`,
+        borderRight: `${size / 2}px solid transparent`,
+        top: '100%',
+        width: 0,
+        borderTop: `${(size * 0.5775) / 2}px solid ${mainColor}`
+      }
+    },
+    bar: {
+      borderRight: `${barSize}px solid ${barColor}`,
+      borderBottomRightRadius: 5,
+      borderTopRightRadius: 5
+    }
+  });
+};
+
+const getBarStyle = (barPercent, mainColor, size) => {
+  return StyleSheet.create({
+    bar: {
+      height: '100%',
+      width: barPercent + '%',
+      backgroundColor: mainColor,
+      zIndex: 10,
+      display: 'inherit',
+      position: 'relative',
+      left: size
+    }
+  });
+};
+
+const styles = StyleSheet.create({
+  content: {
+    zIndex: 100,
+    position: 'absolute',
+    top: -2,
+    left: 7
+  }
+});
 
 export default Hexagon;
--- src/temp/temp.js
+++ src/temp/temp.js
@@ -5,16 +5,33 @@ import Hexagon from '../components/Hexagon/Hexagon';
 import badge from '../assets/badge.svg';
 import trophy from '../assets/trophy.svg';
 
-class Leaderboard extends Component {
+class Temp extends Component {
   render() {
     return (
       <div className='temp-container'>
         <XP level={14} />
-        <Hexagon><img src={badge} width='35' alt='badge' /></Hexagon>
-        <Hexagon bg='red'><img src={trophy} width='35' alt='trophy' /></Hexagon>
+        <Hexagon
+          bar={true}
+          barColor={'#64C7CC'}
+          barPercent={30}
+          barSize={50}
+          mainColor='#177EC1'
+          size={30}>
+          14
+        </Hexagon>
+        <Hexagon
+          bar={true}
+          barColor={'#247BA0'}
+          barPercent={30}
+          barSize={50}
+          mainColor='#64C7CC'
+          size={50}>
+          <img src={badge} width='35' alt='badge' />
+        </Hexagon>
+        <Hexagon mainColor='#FF5151' size={50}><img src={trophy} width='35' alt='trophy' /></Hexagon>
       </div>
     );
   }
 }
 
-export default Leaderboard;
+export default Temp;
