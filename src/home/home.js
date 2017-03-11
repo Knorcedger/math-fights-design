6976c09a6ebc51b820533273d5e3adf934b13260
Ioanna Lampropoulou <ioannalampropoulou@ladmins-mbp-3.home>
2017-03-11 14:33:30 +0000

    icons and home design

--- src/home/home.js
+++ src/home/home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './home.css';
-import XP from '../components/XP/XP';
 import Hexagon from '../components/Hexagon/Hexagon';
 import badge from '../assets/badge.svg';
 import trophy from '../assets/trophy.svg';
+import history from '../assets/history.svg'; // http://www.flaticon.com/free-icon/scroll_302124#term=papyrus&page=1&position=9
+import shop from '../assets/shop.svg'; // http://www.flaticon.com/free-icon/shop_345763#term=shop&page=3&position=15
+import profile from '../assets/profile.svg';
 
 class Leaderboard extends Component {
   render() {
@@ -12,6 +14,15 @@ class Leaderboard extends Component {
         <span className='xp'>
           <span className='text'>{14}</span>
         </span>
+        <div className="circle-div">
+          <div className="circle"></div>
+        </div>
+        <div className="home-menu">
+          <Hexagon bg='red'><img src={profile} width='35' alt='profile' /></Hexagon>
+          <Hexagon><img src={history} width='34' alt='history' /></Hexagon>
+          <Hexagon bg='red'><img src={trophy} width='32' alt='trophy' /></Hexagon>
+          <Hexagon><img src={shop} width='32' alt='shop' /></Hexagon>
+        </div>
         <div className='full-width-div'>
           <span className='semi-circle'></span>
         </div>
--- src/temp/temp.js
+++ src/temp/temp.js
@@ -4,6 +4,9 @@ import XP from '../components/XP/XP';
 import Hexagon from '../components/Hexagon/Hexagon';
 import badge from '../assets/badge.svg';
 import trophy from '../assets/trophy.svg';
+import history from '../assets/history.svg'; // http://www.flaticon.com/free-icon/scroll_302124#term=papyrus&page=1&position=9
+import shop from '../assets/shop.svg'; // http://www.flaticon.com/free-icon/shop_345763#term=shop&page=3&position=15
+import profile from '../assets/profile.svg';
 
 class Leaderboard extends Component {
   render() {
@@ -11,7 +14,10 @@ class Leaderboard extends Component {
       <div className='temp-container'>
         <XP level={14} />
         <Hexagon><img src={badge} width='35' alt='badge' /></Hexagon>
-        <Hexagon bg='red'><img src={trophy} width='35' alt='trophy' /></Hexagon>
+        <Hexagon bg='red'><img src={trophy} width='32' alt='trophy' /></Hexagon>
+        <Hexagon bg='red'><img src={history} width='34' alt='history' /></Hexagon>
+        <Hexagon><img src={shop} width='32' alt='shop' /></Hexagon>
+        <Hexagon bg='red'><img src={profile} width='35' alt='profile' /></Hexagon>
       </div>
     );
   }
