f9ecfa2933b071e37c6b9c363d2fc300f3d48ada
Ioanna Lampropoulou <ioannalampropoulou@ladmins-mbp-3.home>
2017-03-12 00:57:57 +0000

    changes in temp due to hexagon changess

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -4,8 +4,8 @@ import XP from '../components/XP/XP';
 import Hexagon from '../components/Hexagon/Hexagon';
 import badge from '../assets/badge.svg';
 import trophy from '../assets/trophy.svg';
-// import history from '../assets/history.svg'; // http://www.flaticon.com/free-icon/scroll_302124#term=papyrus&page=1&position=9
-// import shop from '../assets/shop.svg'; // http://www.flaticon.com/free-icon/shop_345763#term=shop&page=3&position=15
+import history from '../assets/history.svg'; // http://www.flaticon.com/free-icon/scroll_302124#term=papyrus&page=1&position=9
+import shop from '../assets/shop.svg'; // http://www.flaticon.com/free-icon/shop_345763#term=shop&page=3&position=15
 // import profile from '../assets/profile.svg';
 
 class Temp extends Component {
@@ -18,20 +18,25 @@ class Temp extends Component {
           barColor={'#64C7CC'}
           barPercent={30}
           barSize={50}
+          image={badge}
           mainColor='#177EC1'
-          size={30}>
-          14
+          size={50}>
         </Hexagon>
         <Hexagon
-          bar={true}
-          barColor={'#247BA0'}
-          barPercent={30}
-          barSize={50}
-          mainColor='#64C7CC'
+          mainColor='#295'
+          image={badge}
           size={50}>
-          <img src={badge} width='35' alt='badge' />
         </Hexagon>
-        <Hexagon mainColor='#FF5151' size={50}><img src={trophy} width='35' alt='trophy' /></Hexagon>
+        <Hexagon
+          mainColor='#295'
+          image={shop}
+          size={50}>
+        </Hexagon>
+        <Hexagon
+          mainColor='#295'
+          image={trophy}
+          size={50}>
+        </Hexagon>
       </div>
     );
   }
