867a702bc29bcc46eac494d4979dfc0633531c4e
Achilleas Tsoumitas <knorcedger@gmail.com>
2017-03-29 19:29:57 +0100

    Fixed some linting errors

--- src/home/home.js
+++ src/home/home.js
@@ -5,7 +5,7 @@ import Hexagon from '../components/Hexagon/Hexagon';
 import trophy from '../assets/trophy.svg';
 import history from '../assets/history.svg'; // http://www.flaticon.com/free-icon/scroll_302124#term=papyrus&page=1&position=9
 import shop from '../assets/shop.svg'; // http://www.flaticon.com/free-icon/shop_345763#term=shop&page=3&position=15
-import profile from '../assets/profile.svg';
+// import profile from '../assets/profile.svg';
 import badge from '../assets/badge.svg';
 
 class Leaderboard extends Component {
