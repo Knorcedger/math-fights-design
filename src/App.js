7f82f16f8df67fd4635d6fc56622af68e205c34e
lampropoi <lampropoi@gmail.com>
2017-04-02 00:43:19 +0100

    welcome added in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './home/home';
 import Leaderboard from './leaderboard/leaderboard';
 import Temp from './temp/temp';
+import Welcome from './welcome/welcome';
+
 import {
   BrowserRouter as Router,
   Route,
@@ -18,12 +20,14 @@ class App extends Component {
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/leaderboard'>Leaderboard</Link></li>
             <li><Link to='/temp'>Temp</Link></li>
+            <li><Link to='/welcome'>Welcome</Link></li>
           </ul>
           <hr/>
 
           <Route exact path='/' component={Home} />
           <Route path='/leaderboard' component={Leaderboard} />
           <Route path='/temp' component={Temp} />
+          <Route path='/welcome' component={Welcome} />
         </div>
       </Router>
     );
