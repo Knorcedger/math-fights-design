7d5dc2b6b64db9225c70a76869cbb6c5ddd29c1f
lampropoi <lampropoi@gmail.com>
2017-04-02 02:13:13 +0100

    login added in app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './home/home';
 import Leaderboard from './leaderboard/leaderboard';
 import Temp from './temp/temp';
 import Welcome from './welcome/welcome';
+import Login from './login/login';
 
 import {
   BrowserRouter as Router,
@@ -15,19 +16,24 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/leaderboard'>Leaderboard</Link></li>
-            <li><Link to='/temp'>Temp</Link></li>
-            <li><Link to='/welcome'>Welcome</Link></li>
-          </ul>
+        <div className="App-body">
+          <div className="App-links">
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/leaderboard'>Leaderboard</Link></li>
+              <li><Link to='/temp'>Temp</Link></li>
+              <li><Link to='/welcome'>Welcome</Link></li>
+            </ul>
+            <ul>
+              <li><Link to='/login'>Login</Link></li>
+            </ul>
+          </div>
           <hr/>
-
           <Route exact path='/' component={Home} />
           <Route path='/leaderboard' component={Leaderboard} />
           <Route path='/temp' component={Temp} />
           <Route path='/welcome' component={Welcome} />
+          <Route path='/login' component={Login} />
         </div>
       </Router>
     );
