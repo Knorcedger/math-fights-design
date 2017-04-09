8ac8b4290d6e009fb1ab51eb84d6cadb12ea0844
Achilleas Tsoumitas <knorcedger@gmail.com>
2017-04-09 14:23:45 +0100

    Added keyboard shortcuts to open/close nav

--- src/App.js
+++ src/App.js
@@ -13,22 +13,49 @@ import {
 } from 'react-router-dom';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      openNav: localStorage.getItem('openNav')
+    }
+  }
+
+  componentDidMount() {
+    const self = this;
+    window.onkeyup = function(e) {
+      const key = e.keyCode ? e.keyCode : e.which;
+
+      if (key === 219) {
+        localStorage.setItem('openNav', true);
+        self.setState({openNav: true});
+      } else if (key === 221) {
+        localStorage.setItem('openNav', false);
+        self.setState({openNav: false});
+      }
+    }
+  }
+
   render() {
     return (
       <Router>
-        <div className="App-body">
-          <div className="App-links">
-            <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/leaderboard'>Leaderboard</Link></li>
-              <li><Link to='/temp'>Temp</Link></li>
-              <li><Link to='/welcome'>Welcome</Link></li>
-            </ul>
-            <ul>
-              <li><Link to='/login'>Login</Link></li>
-            </ul>
-          </div>
-          <hr/>
+        <div className='app-body'>
+          {
+            this.state.openNav && (
+              <div className='nav collapsed'>
+                <ul>
+                  <li><Link to='/'>Home</Link></li>
+                  <li><Link to='/leaderboard'>Leaderboard</Link></li>
+                  <li><Link to='/temp'>Temp</Link></li>
+                  <li><Link to='/welcome'>Welcome</Link></li>
+                </ul>
+                <ul>
+                  <li><Link to='/login'>Login</Link></li>
+                </ul>
+              </div>
+            )
+          }
           <Route exact path='/' component={Home} />
           <Route path='/leaderboard' component={Leaderboard} />
           <Route path='/temp' component={Temp} />
