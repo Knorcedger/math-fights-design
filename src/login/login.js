b1d77c49c187feb2a772bba3598a7952d1caf688
Ioanna Lampropoulou <ioannalampropoulou@ladmins-mbp-3.home>
2017-04-30 17:35:28 +0100

    button component added in login.js

--- src/login/login.js
+++ src/login/login.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import './login.css';
+import styled from 'styled-components';
+import logo from '../assets/images/sword.svg';
+import Button from '../components/Button/Button';
+
+const Container = styled.div`
+  background-color: #F25F5C;
+  overflow-x: hidden;
+`;
+const Logo = styled.div`
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  background: linen;
+  width: 25%;
+  margin: auto;
+  padding: 20px;
+  border-radius: 20px;
+  margin-top: 20px;
+  border: 3px solid darkgray;
+  div {
+    border-radius: 50%;
+    width: 80px;
+    background: url(${logo});
+    background-size: contain;
+  }
+`;
+
+const Body = styled.div`
+  background-color: linen;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  div {
+    margin-top: 20px;
+  }
+`;
 
 class Login extends Component {
 
@@ -16,14 +54,14 @@ class Login extends Component {
     const loginClasses = classNames('tab', 'login', {active: this.state.activeTab === 'login'});
     const registerClasses = classNames('tab', 'login', {active: this.state.activeTab === 'register'});
     return (
-      <div className='login-container'>
-        <div className='login-logo-container'>
-          <div className='login-logo'></div>
-        </div>
+      <Container>
+      <Logo>
+        <div/>
+      </Logo>
         <div className='login-full-width-div'>
           <span className='login-semi-circle'></span>
         </div>
-        <div className='login-body'>
+        <Body>
           <div className='tabs'>
             <div className={loginClasses} onClick={() => this.setState({activeTab: 'login'})}>Login</div>
             <div className={registerClasses} onClick={() => this.setState({activeTab: 'register'})}>Register</div>
@@ -33,16 +71,26 @@ class Login extends Component {
             ? (<div className='login box'>
                 <input name='login-username' className='username' placeholder='Username'></input>
                 <input name='login-password' placeholder='Password'></input>
-                <button>Login</button>
+                <Button
+                  backgroundColor='#247BA0'
+                  text='Login'
+                  textColor='#FFFFFF'
+                  width={180}>
+                </Button>
               </div>)
             : (<div className='register box'>
                 <input name='register-username' className='username' placeholder='Username'></input>
                 <input name='register-password' placeholder='Password'></input>
-                <button>Register</button>
+                <Button
+                  backgroundColor='#247BA0'
+                  text='Register'
+                  textColor='#FFFFFF'
+                  width={180}>
+                </Button>
               </div>)
           }
-        </div>
-      </div>
+        </Body>
+      </Container>
     );
   }
 }
