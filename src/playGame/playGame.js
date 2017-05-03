ef82edc26ebf26d2ec47944a0cadb5dc6a216e84
Achilleas Tsoumitas <knorcedger@gmail.com>
2017-05-03 08:16:24 +0100

    Added PlayGame. Not finished

---
+++ src/playGame/playGame.js
@@ -0,0 +1,159 @@
+import React, {Component, PropTypes} from 'react';
+import Button from '../components/Button/Button';
+import Hexagon from '../components/Hexagon/Hexagon';
+import Smile from '../components/Smile/Smile';
+import trophy from '../assets/images/trophy.svg';
+import coin from '../assets/images/coin.svg';
+import styled from 'styled-components';
+import colors from '../modules/colors';
+
+const Container = styled.div`
+  background-color: linen;
+  overflow-x: hidden;
+`;
+const Header = styled.div`
+  padding: 10px 10px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const OpponentInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const MyInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const InfoRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Name = styled.div`
+  padding-left: 10px;
+`;
+
+const Body = styled.div`
+  background-color: ${colors.red};
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+/**
+* Home View
+**/
+class Home extends Component {
+  /**
+   * Render the home view to the user's screen
+   * @return {object} the displayed view
+  **/
+  render() {
+    return (
+      <Container>
+        <Header>
+          <OpponentInfo>
+            <InfoRow>
+              <Hexagon
+              mainColor={colors.blue}
+              size={30}
+              text='14'>
+              </Hexagon>
+              <Name>
+                Leopold
+              </Name>
+            </InfoRow>
+            <Hexagon
+              bar={true}
+              barColor={colors.gray1}
+              barContent={1535}
+              barPercent={0}
+              barSize={80}
+              image={trophy}
+              mainColor={colors.gray}
+              size={30}>
+            </Hexagon>
+          </OpponentInfo>
+          <MyInfo>
+            <InfoRow>
+              <Hexagon
+                mainColor={colors.blue}
+                size={30}
+                text='14'>
+              </Hexagon>
+              <Name>
+                Knorcedger
+              </Name>
+            </InfoRow>
+            <InfoRow>
+              <Hexagon
+                bar={true}
+                barColor={colors.gray1}
+                barContent={1535}
+                barPercent={0}
+                barSize={80}
+                image={trophy}
+                mainColor={colors.gray}
+                size={30}>
+              </Hexagon>
+              <Hexagon
+                bar={true}
+                barColor={colors.gray1}
+                barContent={1535}
+                barPercent={0}
+                barSize={80}
+                image={coin}
+                mainColor={colors.gray}
+                size={30}>
+              </Hexagon>
+            </InfoRow>
+            <InfoRow>
+              <Hexagon
+                bar={true}
+                barColor={colors.gray1}
+                barContent={23}
+                barPercent={0}
+                barSize={80}
+                mainColor={colors.green}
+                size={30}
+                text='+'>
+              </Hexagon>
+              <Hexagon
+                bar={true}
+                barColor={colors.gray1}
+                barContent={27}
+                barPercent={0}
+                barSize={80}
+                mainColor={colors.red}
+                size={30}
+                text='-'>
+              </Hexagon>
+            </InfoRow>
+          </MyInfo>
+        </Header>
+        <Smile colorUp={colors.linen} colorDown={colors.red} />
+        <Body>
+          <Button
+            backgroundColor={colors.yellow}
+            borderColor={colors.gray}
+            text='Start Game!'
+            textColor={colors.gray}
+            width={140}
+            onClick={() => {}}>
+          </Button>
+        </Body>
+      </Container>
+    );
+  }
+};
+
+Home.contextTypes = {
+  history: PropTypes.object
+};
+
+export default Home;
